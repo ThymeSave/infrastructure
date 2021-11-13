d44164198100aa46090a36c736d24d063a1bb899
Timo Reymann <mail@timo-reymann.de>
2021-11-13 20:37:19 +0100

    fix: Don't use nested subdomain for cloudflare cert to work

--- terraform/cloudflare/app.tf
+++ terraform/cloudflare/app.tf
@@ -1,6 +1,7 @@
 resource "cloudflare_record" "funnel" {
-  name    = "funnel.app"
-  type    = "CNAME"
+  name    = "funnel.thymesave.app"
+  type    = "A"
   zone_id = data.cloudflare_zone.thymesave_app.id
   value   = "130.162.34.79"
+  proxied = true
 }
