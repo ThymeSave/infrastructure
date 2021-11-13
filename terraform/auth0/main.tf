1dc7809996738d9da26462e08bfbd2fad355672d
Timo Reymann <mail@timo-reymann.de>
2021-11-13 15:56:23 +0100

    feat: Add initial auth0 config

---
+++ terraform/auth0/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket                      = "terraform-state"
+    key                         = "auth0/terraform.tfstate"
+    region                      = "eu-frankfurt-1"
+    shared_credentials_file     = "../state_credentials.ini"
+    endpoint                    = "https://frqliwzhrsmg.compat.objectstorage.eu-frankfurt-1.oraclecloud.com"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    force_path_style            = true
+  }
+
+  required_providers {
+    auth0 = {
+      source  = "alexkappa/auth0"
+      version = "~> 0.24"
+    }
+  }
+}
+
+provider "auth0" {
+  domain        = "thymesave.eu.auth0.com"
+  client_id     = "hWDtJtFz7VYn3JvTRR732hKqte0dw44B"
+  client_secret = var.api_client_secret
+}
---
+++ terraform/auth0/variables.tf
@@ -0,0 +1,4 @@
+variable "api_client_secret" {
+  description = "Client-Secret to use for Auth0 API in provider"
+  type        = string
+}
