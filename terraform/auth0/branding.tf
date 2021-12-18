81db600836a247fbaf8fc9bd746ee727e4e460f1
Timo Reymann <mail@timo-reymann.de>
2021-12-19 00:05:44 +0100

    feat(#58): Add auth0 config (#3)
    * feat: Add initial auth0 config
    
    * feat: Add setup for funnel (#2)
    
    * feat: Add initial funnel setup
    
    The role will be extracted and separately installable via galaxy. Till the project reaches a stable state, the role will reside here.
    
    * feat: Add load balancer for funnel
    
    * feat: Add cname for funnel service
    
    * fix: Don't use nested subdomain for cloudflare cert to work
    
    * feat: Add app subdomain
    
    * chore: Upgrade funnel to 0.0.3
    
    * feat(#58): Add auth0 client for silverware

---
+++ terraform/auth0/branding.tf
@@ -0,0 +1,7 @@
+resource "auth0_branding" "this" {
+  logo_url = "https://design.thymesave.app/logo/color/ThymeSaveLogo.svg"
+  colors {
+    primary         = "#0c7c5c"
+    page_background = "#000000"
+  }
+}
---
+++ terraform/auth0/outputs.tf
@@ -0,0 +1,4 @@
+output "silverware_prod__client_id" {
+  value       = auth0_client.silverware_prod.client_id
+  description = "Client ID of the silverware client"
+}
